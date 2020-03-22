c3377db7270311bae8a185cb6c232744ae24ea82
ShafayetRajit <62207851+ShafayetRajit@users.noreply.github.com>
2020-03-22 23:12:25 +0600

    Update header.php

--- header.php
+++ header.php
@@ -71,9 +71,12 @@
                               <li class="nav-item">
                                   <a class="nav-link" href="index.php#team_part">Teams</a>
                               </li>
+                              <li class="nav-item">
+                                  <a class="nav-link" href="index.php#">Log In</a>
+                              </li>
                           </ul>
                       </div>
-                      <div class="join_us"><a href="#">Join Us</a></div>
+                      <div class="join_us"><a href="#">Apply</a></div>
                   </nav>
               </div>
           </div>
@@ -81,11 +84,11 @@
   </header>
   <!--    Header Part Ends-->
     <section id="banner_part">
-        <div class="banner">
+        <div class="banner h-50">
             <div class="overlay">
                 <div class="container">
                     <div class="row">
-                        <div class="col-lg-12">
+                        <div class="col-lg-12 p-5">
                             <div class="box">
                                 <h2>Welcome to</h2>
                                 <h1>NSU ACM SC</h1>
